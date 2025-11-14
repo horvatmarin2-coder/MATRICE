11cdb960478f464f561d6de9861652fec6179f06
Fake ime <crnojamatej66@gmail.com>
2025-11-15 00:16:33 +0100

    Update kod.py

--- kod.py
+++ kod.py
@@ -1,10 +1,17 @@
+def zbroj():
+    return 3
 import tkinter as tk
 
 aplikacija = tk.Tk()
-
 aplikacija.title("Calculomatrix")
+aplikacija.geometry("1920x1080")
+
+
 
 label = tk.Label(aplikacija,text="Matrica")
 label.pack()
+
+button = tk.Button(aplikacija, text="Izračunaj",width=20, height=2)
+button.place(x=100, y=100)
 
 aplikacija.mainloop()
