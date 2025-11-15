7d74273c64cd13b7eb34eb41a431b31c6c75ac36
Marin <horvatmarin2@gmail.com>
2025-11-15 20:34:02 +0100

    Update MATRICE.py

--- MATRICE.py
+++ MATRICE.py
@@ -6,7 +6,7 @@ aplikacija.title("MATRICE zadatak")
 aplikacija.geometry("1000x600")
 aplikacija.configure(bg="#aa90bb")
 
-label = tk.Label(aplikacija, text="MATRICE",bg="#aa90bb", font=("Arial", 34))
+label = tk.Label(aplikacija, text="MATRICE",bg="#aa90bb", font=("Minecraftia", 34))
 label.pack(pady=40)
 
 aplikacija.mainloop()
